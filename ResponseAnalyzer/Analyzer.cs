27b3fe1bdc28fb9f87e1235905d5721d6b03e725
Dominik Fuchß <fuchss@escde.net>
2019-11-12 12:43:32 +0100

    Added more blacklisted words to Response Analyzer

--- ResponseAnalyzer/Analyzer.cs
+++ ResponseAnalyzer/Analyzer.cs
@@ -118,7 +118,7 @@ namespace Framework.ResponseAnalyzer
         {
             List<Tuple<T, float>> result = new List<Tuple<T, float>>();
 
-            var split = Regex.Matches(_response, "[A-Za-z0-9äöüÄÖÜß_\\.]+");
+            var split = Regex.Matches(_response, "[A-Za-z0-9äöüÄÖÜß_\\-\\.]+");
             var tokens = split.Select(t => t.Value).Where(t => !_blackList.Any(b => b.ToLower() == t.ToLower())).ToList();
 
             float max = 0;
